ab765af8b6d9aefdda43179594b070484a17c8c9
PramukhNadig <pnadig1@gmail.com>
2022-02-09 04:07:01 -0500

    first commit

---
+++ labs/lab0128/1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+count=0
+for args in $@; do
+    #count=$(( $count + 1 ))
+    let count=count+1
+done
---
+++ labs/lab0128/2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "My home dir is:" /home/$(whoami)
+echo "My home dir is " $HOME
